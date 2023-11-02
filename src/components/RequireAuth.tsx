adfc3b62910400261a64e6f586a53016a0fe0768
FREDRICK KYEKI <fredrickisaac142@gmail.com>
2023-11-02 11:40:33 +0300

    [fix]: admin authentication

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -7,9 +7,9 @@ export const RequireAuth = ({ children }: childrenProps) => {
   const { currentUser } = useAuth() as any;
   const location = useLocation();
   const tok = JSON.parse(JSON.parse(localStorage.getItem("user_tk") as string));
-  const admin_tok = localStorage.getItem("admin_tk") as string;
+  // const admin_tok = localStorage.getItem("admin_tk") as string;
 
-  if (!currentUser?.email && !tok?.email && !admin_tok) {
+  if (!currentUser?.email && !tok?.email) {
     return <Navigate to={routes.login} state={location.pathname} />;
   }
 
